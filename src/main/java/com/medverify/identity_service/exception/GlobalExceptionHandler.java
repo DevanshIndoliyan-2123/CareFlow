77926092fc292329e1bf1836b247a5965edeca34
Devansh647 <devansh21232123@gmail.com>
2026-08-17 22:57:13 +0530

    Add identity service

--- src/main/java/com/medverify/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/medverify/identity_service/exception/GlobalExceptionHandler.java
@@ -4,13 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<Map<String, Object>> handleUserAlreadyExists(
