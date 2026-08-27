492d52e320aec0a7cfd24d0177b050a642f2cec2
Aman Singh <amangusain9368@gmail.com>
2026-08-27 20:43:18 +0530

    Fix frontend errors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './features/auth/AuthContext';
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 
 const queryClient = new QueryClient({
   defaultOptions: {
